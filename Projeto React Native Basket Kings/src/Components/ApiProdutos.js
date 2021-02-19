5b39693af519eecc468adb7c0347f75164b08a82
Jonathan <72957709+jgssiqueira@users.noreply.github.com>
2021-02-19 19:55:19 -0300

    Add files via upload

---
+++ Projeto React Native Basket Kings/src/Components/ApiProdutos.js
@@ -0,0 +1,46 @@
+
+
+const ApiProdutos = [
+    {
+        id: Math.random(),
+        produto: 'Los Angeles Lakers',
+        descricao: 'Jerseys',
+        preco: 200.00,
+        precoAnterior: 300.00,
+        imagem: 'lalakers.jpeg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Los Angeles Clippers',
+        descricao: 'Jerseys',
+        preco: 200.00,
+        precoAnterior: 300.00,
+        imagem: 'laclippers.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Miami Heat',
+        descricao: 'Jerseys',
+        preco: 200.00,
+        precoAnterior: 300.00,
+        imagem: 'miamiheat.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Chicago Bulls',
+        descricao: 'Jerseys',
+        preco: 200.00,
+        precoAnterior: 300.00,
+        imagem: 'chicagobulls.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Golden State Warriors',
+        descricao: 'Jerseys',
+        preco: 200.00,
+        precoAnterior: 300.00,
+        imagem: 'gsw.jpg'
+    }
+
+]
+export default ApiProdutos;
